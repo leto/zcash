1f1eef2844d50cf8fa1140a7e6d37db3c0fa6ef1
Jayson Jacobs <j4ys0n@gmail.com>
2019-12-15 14:40:55 -0700

    block height +1

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -878,11 +878,11 @@ UniValue getblocksubsidy(const UniValue& params, bool fHelp)
         );
 
     LOCK(cs_main);
-    int nHeight = (params.size()==1) ? params[0].get_int() : chainActive.Height();
+    int nHeight = (params.size()==1) ? params[0].get_int() : (chainActive.Height() + 1);
     if (nHeight < 0)
         throw JSONRPCError(RPC_INVALID_PARAMETER, "Block height out of range");
 
-    CAmount nReward = GetBlockSubsidy(nHeight + 1, Params().GetConsensus());
+    CAmount nReward = GetBlockSubsidy(nHeight, Params().GetConsensus());
     CAmount nFoundersReward = 0;
     if ((nHeight > 0) && (nHeight <= Params().GetConsensus().GetLastFoundersRewardBlockHeight())) {
         nFoundersReward = nReward * 0.048;
